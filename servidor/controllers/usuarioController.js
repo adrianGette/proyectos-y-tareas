950b049c8d05430aa01858be151d5d28a1e1eec0
Adrian Gette <adrian.gette@icloud.com>
2021-06-22 18:43:41 -0300

    Revisar usuarios unicos

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,8 +1,17 @@
 const Usuario = require('../models/Usuario');
 
 exports.crearUsuario = async (req, res) => {
+
+    // extraer email y password
+    const { email, password } = req.body;
+
     try {
-        let usuario;
+        // revisar que el usuario registrado sea único
+        let usuario = await Usuario.findOne({ email });
+
+        if(usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe' });
+        }
 
         // crea el nuevo usuario+
         usuario = new Usuario(req.body);
@@ -11,7 +20,7 @@ exports.crearUsuario = async (req, res) => {
         await usuario.save();
 
         // mensaje de confirmación
-        res.send('Usuario creado correctamente');
+        res.json({ msg: 'Usuario creado correctamente' });
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
