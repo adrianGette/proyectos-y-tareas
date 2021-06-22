83bbb8dc938fbc5e0984ce1be0323a692dec3c68
Adrian Gette <adrian.gette@icloud.com>
2021-06-22 16:52:23 -0300

    Agregar controlador para crear usuarios

---
+++ servidor/controllers/usuarioController.js
@@ -0,0 +1,5 @@
+
+
+exports.crearUsuario = (req, res) => {
+    console.log("Desde crearUsuario");
+}
--- servidor/routes/usuarios.js
+++ servidor/routes/usuarios.js
@@ -1,10 +1,11 @@
 // rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
 
 // crea un usuario 
 // api/usuarios
-router.post('/', () => {
-
-});
+router.post('/', 
+    usuarioController.crearUsuario
+);
 module.exports = router;
