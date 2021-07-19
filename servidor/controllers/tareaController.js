655cd8834f73872d3e099c58b0d0d188fd116ab2
Adrian Gette <adrian.gette@icloud.com>
2021-07-19 18:55:48 -0300

    Insertar tareas y el proyecto al que pertenecen

--- servidor/controllers/tareaController.js
+++ servidor/controllers/tareaController.js
@@ -9,4 +9,30 @@ exports.crearTarea = async (req, res) => {
     if( !errores.isEmpty() ) {
         return res.status(400).json({ errores: errores.array() })
     }
+
+    
+
+    try {
+
+        // extraer el proyecto y comprobar si existe
+        const { proyecto } = req.body;
+
+        const existeProyecto = await Proyecto.findById(proyecto);
+        if(!existeProyecto) {
+            return res.status(404).json({msg: 'Proyecto no encontrado'});
+        }
+
+        // revisar si el proyecto actual pertenece al usuario autenticado
+        if(existeProyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({msg: 'No autorizado'});
+        }
+
+        // crear la tarea
+        const tarea = new Tarea(req.body);
+        await tarea.save();
+        res.json({ tarea });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
--- servidor/routes/tareas.js
+++ servidor/routes/tareas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const tareaController = require('../controllers/proyectoController');
+const tareaController = require('../controllers/tareaController');
 const auth = require('../middleware/auth');
 const { check } = require('express-validator');
 
@@ -11,7 +11,8 @@ const { check } = require('express-validator');
 router.post('/',
     auth,
     [
-        check('nombre', 'El nombre es obligatorio.').not().isEmpty()
+        check('nombre', 'El nombre es obligatorio.').not().isEmpty(),
+        check('proyecto', 'El proyecto es obligatorio.').not().isEmpty()
     ],
     tareaController.crearTarea
 
