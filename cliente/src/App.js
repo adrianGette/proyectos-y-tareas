8f112a6162bca254c32a974f7702b1e4f50b8e39
Adrian Gette <adrian.gette@icloud.com>
2021-07-21 17:22:33 -0300

    Autenticar al usuario aunque se recargue la app

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -6,16 +6,20 @@ import Proyectos from './components/proyectos/Proyectos';
 
 import ProyectoState from './context/proyectos/proyectoState';
 import TareaState from './context/tareas/tareaState';
-
 import AlertaState from './context/alertas/alertaState';
-
 import AuthState from './context/autenticacion/authState';
+import tokenAuth from './config/token';
+
+
+// revisar si tenemos un token
+const token = localStorage.getItem('token');
+if(token) {
+  tokenAuth(token);
+}
 
 
 function App() {
 
-  console.log(process.env.REACT_APP_BACKEND_URL);
-  
     return (
       <ProyectoState>
         <TareaState>
--- cliente/src/context/autenticacion/authState.js
+++ cliente/src/context/autenticacion/authState.js
@@ -107,7 +107,8 @@ const AuthState = props => {
                 usuario: state.usuario,
                 mensaje: state.mensaje,
                 registrarUsuario,
-                iniciarSesion
+                iniciarSesion,
+                usuarioAutenticado
             }}
         >{props.children}
 
