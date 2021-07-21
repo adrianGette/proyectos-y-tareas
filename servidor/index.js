cb64815f9749c5af94033bf6e46e9b01179eee6e
Adrian Gette <adrian.gette@icloud.com>
2021-07-21 14:19:37 -0300

    Habilitar Cors y crear usuario

--- servidor/index.js
+++ servidor/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 // crear el servidor
 const app = express();
@@ -7,11 +8,14 @@ const app = express();
 // conectar a la base de datos
 conectarDB();
 
+// habilitar cors
+app.use(cors());
+
 // habilitar express.json
 app.use(express.json({ extended: true }));
 
 // puerto de la app
-const  PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
